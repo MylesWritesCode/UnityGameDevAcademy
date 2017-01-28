096491fc1f372f1643fac2e584cc4a1d031c3b88
Myles Berueda <myles.berueda@gmail.com>
2017-01-27 16:46:14 -1000

    Finished List and Arrays Lesson

--- C#Basics/Assets/Conditionals.cs
+++ C#Basics/Assets/Conditionals.cs
@@ -2,15 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Conditionals : MonoBehaviour {
-
+public class Conditionals : MonoBehaviour
+{
+    int attackerTowersRemaining = 2;
+    bool attackerMainTowerDestroyed = false;
+    int enemyTowersRemaining = 2;
+    bool enemyMainTowerDestroyed = false;
+    float timer = 200;
+    bool gameOver = false;
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        if (attackerMainTowerDestroyed || enemyMainTowerDestroyed)
+        {
+            if (attackerMainTowerDestroyed)
+            {
+                Debug.Log("Defender Wins by Main Tower Kill");
+            } 
+            else
+            {
+                Debug.Log("Attacker Wins by Main Tower Kill");
+            }
+        }
+        else if (timer <= 0)
+        {
+            gameOver = true;
+        }
+
+        if (gameOver)
+        {
+            if (attackerTowersRemaining > enemyTowersRemaining)
+            {
+                Debug.Log("Attacker Wins.");
+            }
+            else
+            {
+                Debug.Log("Defender Wins.");
+            }
+        }
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
 }
---
+++ C#Basics/Assets/ListsAndArrays.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListsAndArrays : MonoBehaviour
+{
+    // This is one way to declare all the players in the game. 
+    //string player1 = "Myles";
+    //string player2 = "Mel";
+    //string player3 = "Eric";
+    //string player4 = "Amanda";
+    
+    // This is a better way to initialize an array.
+    string[] players = { "Myles", "Mel", "Eric", "Amanda" };
+
+    public GameObject cubePrefab;
+    GameObject[] cubes = new GameObject[5];
+    
+
+	// Use this for initialization
+	void Start ()
+    {
+        // This is a dumb way to call on each person in the list.
+        // Debug.Log("Welcome: " + player1);
+        // Debug.Log("welcome: " + player2);
+        // Debug.Log("Welcome: " + player3);
+        // Debug.Log("Welcome: " + player4);
+
+        // Better way to do the above.
+        // Debug.Log("Player one: " + players[0]);
+
+        for (int i = 0; i < cubes.Length; i++)
+        {
+            GameObject cube = Instantiate(cubePrefab);
+            cubes[i] = cube;
+            cube.transform.position = new Vector3(i, 2*i, i);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
