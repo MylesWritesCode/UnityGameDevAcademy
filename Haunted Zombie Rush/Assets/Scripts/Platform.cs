ffb8a7eae29a1641005b7e42337d698b4dfa35c6
Myles Berueda <myles.berueda@gmail.com>
2017-01-28 14:55:28 -1000

    Created background, front, and imported models with animations.

---
+++ Haunted Zombie Rush/Assets/Scripts/Platform.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour 
+{
+	[SerializeField] private float objectSpeed = 4;
+	[SerializeField] private float resetPosition = 51.0f;
+	[SerializeField] private float startPosition = -53f;
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	protected virtual void Update () 
+	{
+		transform.Translate(Vector3.right * (objectSpeed * Time.deltaTime));
+
+		if (transform.localPosition.x >= resetPosition)
+		{
+			Vector3 newPos = new Vector3(startPosition, transform.position.y, transform.position.z);
+			transform.position = newPos;
+		}
+	}
+}
---
+++ Haunted Zombie Rush/Assets/Scripts/Rock.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : Platform 
+{
+	[SerializeField] Vector3 topPosition;
+	[SerializeField] Vector3 bottomPosition;
+	[SerializeField] float rockSpeed;
+
+	// Use this for initialization
+	void Start () 
+	{
+		StartCoroutine(Move(topPosition));
+	}
+
+	protected override void Update()
+	{
+		base.Update();
+	}
+
+	IEnumerator Move(Vector3 target)
+	{
+		while (Mathf.Abs((target - transform.localPosition).y) > 0.20f)
+		{
+			Vector3 direction = target.y == topPosition.y ? Vector3.up : Vector3.down;
+			transform.localPosition += direction * (rockSpeed * Time.deltaTime);
+
+			yield return null;
+		}
+		Debug.Log("Reached target.");
+		yield return new WaitForSeconds(0.5f);
+
+		Vector3 newTarget = target.y == topPosition.y ? bottomPosition : topPosition;
+		StartCoroutine(Move(newTarget));
+	}
+}
