65032dd66e494caa2ec74d8bdf64015bbd6564cd
Myles Berueda <myles.berueda@gmail.com>
2017-02-01 01:51:05 -1000

    Added TowerManager and buttons.

---
+++ Tower Defense/Assets/Scripts/TowerManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class TowerManager : Singleton<TowerManager> 
+{
+	// Type of TowerBtn from /Towers/TowerBtn.cs
+	private TowerBtn towerBtnPressed;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	public void selectedTower(TowerBtn towerSelected)
+	{
+		towerBtnPressed = towerSelected;
+		Debug.Log(towerBtnPressed.gameObject);
+	}
+}
