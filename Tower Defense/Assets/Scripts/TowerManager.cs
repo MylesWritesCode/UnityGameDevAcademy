a1cd886e0a4cd583932ca5ce7fc333db041d4a61
Myles Berueda <myles.berueda@gmail.com>
2017-02-05 12:49:20 -1000

    Comments.

--- Tower Defense/Assets/Scripts/TowerManager.cs
+++ Tower Defense/Assets/Scripts/TowerManager.cs
@@ -13,24 +13,33 @@ public class TowerManager : Singleton<TowerManager>
 	}
 	
 	// Update is called once per frame
-	void Update () 
+	void Update ()
 	{
+		// Getting input needs to be in Update(). If left-click on screen...
 		if (Input.GetMouseButtonDown(0))
 		{
+			// Find the position of click and set to mapPoint.
 			Vector2 mapPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			// Find the distance between (0, 0) and the point that was touched (clicked). Set to hit.
 			RaycastHit2D hit = Physics2D.Raycast(mapPoint, Vector2.zero);
+			// If you're allowed to build on the hit point...
 			if(hit.collider.tag == "Build Site")
 			{
+				// Run PlaceTower() on hit (pos of click).
 				PlaceTower(hit);
 			}
 		}
 	}
 
+	// Uses Raycast to place tower on map.
 	public void PlaceTower(RaycastHit2D hit)
 	{
+		// If touch (click) is over a game object and tower button is pushed...
 		if(!EventSystem.current.IsPointerOverGameObject() && towerBtnPressed != null)
 		{
+			// Instantiate tower based on which button is pushed. TowerObject from TowerBtn.cs.
 			GameObject newTower = Instantiate(towerBtnPressed.TowerObject);
+			// Move TowerObject to raycasted position.
 			newTower.transform.position = hit.transform.position;
 		}
 	}
@@ -38,6 +47,5 @@ public class TowerManager : Singleton<TowerManager>
 	public void selectedTower(TowerBtn towerSelected)
 	{
 		towerBtnPressed = towerSelected;
-		Debug.Log(towerBtnPressed.gameObject);
 	}
 }
