3588c797b156f3f212f496dfb496949269289d49
Myles Berueda <myles.berueda@gmail.com>
2017-02-07 16:38:02 -1000

    Added Projectile.cs

---
+++ Tower Defense/Assets/Scripts/Projectile.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public enum proType
+{
+	rock, arrow, fireball
+};
+
+public class Projectile : MonoBehaviour 
+{
+	[SerializeField] private int attackDmg;
+	[SerializeField] private proType projectileType;
+
+	public int AttackDmg 
+	{
+		get 
+		{
+			return attackDmg;
+		}
+	}
+
+	public proType ProjectileType
+	{
+		get
+		{
+			return projectileType;
+		}
+	}
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
