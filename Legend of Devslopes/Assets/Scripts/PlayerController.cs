97d6de8ed58e5b8acd352f1cd18fe26fe05eb16e
Myles Berueda <myles.berueda@gmail.com>
2017-03-06 11:46:22 -1000

    Added box colliders.

---
+++ Legend of Devslopes/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour 
+{
+	[SerializeField] private float moveSpeed = 10f;
+
+	private CharacterController characterController;
+
+	// Use this for initialization
+	void Start () 
+	{
+		// Look inside of the GameObject and get a Character Controller.
+		characterController = GetComponent<CharacterController>();	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// Get inputs from horizontal and vertical axis. 
+		Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		characterController.SimpleMove(moveDirection * moveSpeed);
+	}
+}
