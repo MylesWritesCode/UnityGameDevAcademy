710b267564f21b71781436be89960c1e6e7c56d5
Myles Berueda <myles.berueda@gmail.com>
2017-03-06 11:50:44 -1000

    Added camera follow script.

--- Legend of Devslopes/Assets/Scripts/CameraFollow.cs
+++ Legend of Devslopes/Assets/Scripts/CameraFollow.cs
@@ -1,16 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
-public class CameraFollow : MonoBehaviour {
+public class CameraFollow : MonoBehaviour 
+{
+	[SerializeField] Transform target;
+	// Smooth camera movement.
+	[SerializeField] float smoothing = 5f;
+	Vector3 offset;
+
+	void Awake()
+	{
+		Assert.IsNotNull(target);
+	}
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start () 
+	{
+		offset = transform.position - target.position;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () 
+	{
+		Vector3 targetCameraPosition = target.position + offset;
+		// Linear interpolation between two vectors.
+		transform.position = Vector3.Lerp(transform.position, targetCameraPosition, smoothing * Time.deltaTime);
 	}
 }
