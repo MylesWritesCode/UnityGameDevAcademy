4b7f85d46debeef9ba4c771a9e1c2a78155cdd8f
Myles Berueda <myles.berueda@gmail.com>
2017-03-07 12:44:32 -1000

    Added rotations to camera.

--- Legend of Devslopes/Assets/Scripts/PlayerController.cs
+++ Legend of Devslopes/Assets/Scripts/PlayerController.cs
@@ -1,12 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class PlayerController : MonoBehaviour 
 {
 	[SerializeField] private float moveSpeed = 10f;
+	[SerializeField] private LayerMask layerMask;
 
 	private CharacterController characterController;
+	private Vector3 currentLookTarget = Vector3.zero;
+
+	void Awake()
+	{
+
+	}
 
 	// Use this for initialization
 	void Start () 
@@ -21,5 +29,30 @@ public class PlayerController : MonoBehaviour
 		// Get inputs from horizontal and vertical axis. 
 		Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 		characterController.SimpleMove(moveDirection * moveSpeed);
+	}
+
+	void FixedUpdate()
+	{
+		RaycastHit hit;
+		// Create the ray from the camera to the mouse position.
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		// See the ray being cast. 
+		Debug.DrawRay(ray.origin, ray.direction * 500, Color.blue);
+		// Check raycast hit points, if there are other physics collider triggers, don't set them off. 
+		if (Physics.Raycast(ray, out hit, 500, layerMask, QueryTriggerInteraction.Ignore))
+		{
+			// If camera isn't already looking at target...
+			if (hit.point != currentLookTarget)
+			{
+				// Make the point that our mouse is looking the currentLookTarget.
+				currentLookTarget = hit.point;
+			}
+			// Get the new target position.
+			Vector3 targetPosition = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+			// Get the angle of rotation from where it already is, to where it's supposed to be.
+			Quaternion rotation = Quaternion.LookRotation(targetPosition - transform.position);
+			// Initiate rotation between the angle it's currently rotated, the new rotation, by Time.deltaTime * 10f
+			transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * 10f);
+		}
 	}
 }
