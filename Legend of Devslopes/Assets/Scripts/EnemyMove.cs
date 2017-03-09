a859bd336a36f30584f942140c7192be805e92ab
Myles Berueda <myles.berueda@gmail.com>
2017-03-08 19:13:42 -1000

    Start script for AI.

---
+++ Legend of Devslopes/Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMove : MonoBehaviour 
+{
+	[SerializeField] Transform player;
+	private NavMeshAgent nav;
+	private Animator anim;
+	// Use this for initialization
+	void Start () 
+	{
+		anim = GetComponent<Animator>();
+		nav = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
