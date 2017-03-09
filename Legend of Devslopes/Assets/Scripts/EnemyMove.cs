47084d4f35521202aa9fef17b56aed16486bacf6
Myles Berueda <myles.berueda@gmail.com>
2017-03-08 19:15:09 -1000

    Added AI Move

--- Legend of Devslopes/Assets/Scripts/EnemyMove.cs
+++ Legend of Devslopes/Assets/Scripts/EnemyMove.cs
@@ -2,12 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Assertions;
 
 public class EnemyMove : MonoBehaviour 
 {
 	[SerializeField] Transform player;
 	private NavMeshAgent nav;
 	private Animator anim;
+
+	void Awake()
+	{
+		Assert.IsNotNull(player);
+	}
 	// Use this for initialization
 	void Start () 
 	{
@@ -18,6 +24,6 @@ public class EnemyMove : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-		
+		nav.SetDestination(player.position);
 	}
 }
