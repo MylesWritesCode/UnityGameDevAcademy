b37fe5fd69b5f4b5f60c5298d3ffba4bd0bc3b1b
Myles Berueda <myles.berueda@gmail.com>
2017-03-10 12:09:25 -1000

    Enemy attack variables.

---
+++ Legend of Devslopes/Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour 
+{
+	[SerializeField] private float range = 3f;
+	[SerializeField] private float attackSpeed = 1f;
+
+	private Animator anim;
+	private GameObject player;
+	private bool playerInRange;
+	private BoxCollider[] weaponColliders;
+
+	// Use this for initialization
+	void Start () 
+	{
+		anim = GetComponent<Animator>();
+		player = GameManager.instance.Player;
+		weaponColliders = GetComponentsInChildren<BoxCollider>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
