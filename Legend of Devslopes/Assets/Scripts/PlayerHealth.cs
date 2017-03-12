b95b357f971e3382f6fded1da55faa27040b0d24
Myles Berueda <myles.berueda@gmail.com>
2017-03-11 16:11:40 -1000

    Added variables to player health>

--- Legend of Devslopes/Assets/Scripts/PlayerHealth.cs
+++ Legend of Devslopes/Assets/Scripts/PlayerHealth.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour 
 {
+	[SerializeField] int startingHealth = 100;
+	[SerializeField] float timeSinceLastHit = 2f;
 
+	private float timer = 0f;
+	private CharacterController characterController;
+	private Animator anim;
+	private int currentHealth;
 	// Use this for initialization
 	void Start () 
 	{
-		
+		anim = GetComponent<Animator>();
+		characterController = GetComponent<CharacterController>();
+		currentHealth = startingHealth;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		
+		timer += Time.deltaTime;
 	}
 }
