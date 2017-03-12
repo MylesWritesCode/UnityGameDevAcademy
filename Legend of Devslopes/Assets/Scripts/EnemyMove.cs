445c969d305807f5e744f874343fe7de08b2afa2
Myles Berueda <myles.berueda@gmail.com>
2017-03-11 16:21:24 -1000

    Bugfixes for EnemyMove.

--- Legend of Devslopes/Assets/Scripts/EnemyMove.cs
+++ Legend of Devslopes/Assets/Scripts/EnemyMove.cs
@@ -24,6 +24,14 @@ public class EnemyMove : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-		nav.SetDestination(player.position);
+		if (!GameManager.instance.GameOver)
+		{
+			nav.SetDestination(player.position);
+		}
+		else
+		{
+			nav.enabled = false;
+			anim.Play("Idle");
+		}
 	}
 }
