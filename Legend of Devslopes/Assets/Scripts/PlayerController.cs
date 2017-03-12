d9321d470f06db50d00ddcffc4a514f124bcd269
Myles Berueda <myles.berueda@gmail.com>
2017-03-11 16:23:44 -1000

    Bugfixes in PlayerController on death.

--- Legend of Devslopes/Assets/Scripts/PlayerController.cs
+++ Legend of Devslopes/Assets/Scripts/PlayerController.cs
@@ -29,59 +29,64 @@ public class PlayerController : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-		// Get inputs from horizontal and vertical axis. 
-		Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-		characterController.SimpleMove(moveDirection * moveSpeed);
-
-		if (moveDirection == Vector3.zero)
+		if (!GameManager.instance.GameOver)
 		{
-			anim.SetBool("IsWalking", false);
-		}
-		else
-		{
-			anim.SetBool("IsWalking", true);
-		}
+			// Get inputs from horizontal and vertical axis. 
+			Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			characterController.SimpleMove(moveDirection * moveSpeed);
 
-		// If click...
-		if (Input.GetMouseButtonDown(0))
-		{
-			// Basic attack.
-			anim.Play("DoubleChop");
-			// Some other code here to damage based on collider hit.
-		}
+			if (moveDirection == Vector3.zero)
+			{
+				anim.SetBool("IsWalking", false);
+			}
+			else
+			{
+				anim.SetBool("IsWalking", true);
+			}
 
-		// If click...
-		if (Input.GetMouseButtonDown(1))
-		{
-			// Spin attack animation.
-			anim.Play("SpinAttack");
-			// Some other code to damage based on collider hit.
-		}
+			// If click...
+			if (Input.GetMouseButtonDown(0))
+			{
+				// Basic attack.
+				anim.Play("DoubleChop");
+				// Some other code here to damage based on collider hit.
+			}
 
+			// If click...
+			if (Input.GetMouseButtonDown(1))
+			{
+				// Spin attack animation.
+				anim.Play("SpinAttack");
+				// Some other code to damage based on collider hit.
+			}
+		}
 	}
 
 	void FixedUpdate()
 	{
-		RaycastHit hit;
-		// Create the ray from the camera to the mouse position.
-		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-		// See the ray being cast. 
-		Debug.DrawRay(ray.origin, ray.direction * 500, Color.blue);
-		// Check raycast hits points, if there are other physics collider triggers, don't set them off. 
-		if (Physics.Raycast(ray, out hit, 500, layerMask, QueryTriggerInteraction.Ignore))
+		if (!GameManager.instance.GameOver)
 		{
-			// If camera isn't already looking at target...
-			if (hit.point != currentLookTarget)
+			RaycastHit hit;
+			// Create the ray from the camera to the mouse position.
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			// See the ray being cast. 
+			Debug.DrawRay(ray.origin, ray.direction * 500, Color.blue);
+			// Check raycast hits points, if there are other physics collider triggers, don't set them off. 
+			if (Physics.Raycast(ray, out hit, 500, layerMask, QueryTriggerInteraction.Ignore))
 			{
-				// Make the point that our mouse is looking the currentLookTarget.
-				currentLookTarget = hit.point;
+				// If camera isn't already looking at target...
+				if (hit.point != currentLookTarget)
+				{
+					// Make the point that our mouse is looking the currentLookTarget.
+					currentLookTarget = hit.point;
+				}
+				// Get the new target position.
+				Vector3 targetPosition = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+				// Get the angle of rotation from where it already is, to where it's supposed to be.
+				Quaternion rotation = Quaternion.LookRotation(targetPosition - transform.position);
+				// Initiate rotation between the angle it's currently rotated, the new rotation, by Time.deltaTime * 10f
+				transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * 10f);
 			}
-			// Get the new target position.
-			Vector3 targetPosition = new Vector3(hit.point.x, transform.position.y, hit.point.z);
-			// Get the angle of rotation from where it already is, to where it's supposed to be.
-			Quaternion rotation = Quaternion.LookRotation(targetPosition - transform.position);
-			// Initiate rotation between the angle it's currently rotated, the new rotation, by Time.deltaTime * 10f
-			transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * 10f);
 		}
 	}
 }
