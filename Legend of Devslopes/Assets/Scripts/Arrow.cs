71a5c517b4cf76a55389226f164d3ebf0fe1cad4
Myles Berueda <myles.berueda@gmail.com>
2017-03-22 23:30:54 -1000

    Create/Destroy arrows.

--- Legend of Devslopes/Assets/Scripts/Arrow.cs
+++ Legend of Devslopes/Assets/Scripts/Arrow.cs
@@ -2,15 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Arrow : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+public class Arrow : MonoBehaviour 
+{
+	void OnCollisionEnter(Collision collider)
+	{
+		Destroy(gameObject);
 	}
 }
