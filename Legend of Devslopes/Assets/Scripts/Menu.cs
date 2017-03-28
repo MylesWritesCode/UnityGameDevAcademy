f132061bce1389b4dff19f1ecb66256fa63fdab4
Myles Berueda <myles.berueda@gmail.com>
2017-03-28 00:35:54 -1000

    Linked up menu buttons.

--- Legend of Devslopes/Assets/Scripts/Menu.cs
+++ Legend of Devslopes/Assets/Scripts/Menu.cs
@@ -1,16 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour {
 
+	[SerializeField] GameObject hero;
+	[SerializeField] GameObject soldier;
+	[SerializeField] GameObject ranger;
+	[SerializeField] GameObject tanker;
+
+	private Animator heroAnim;
+	private Animator soldierAnim;
+	private Animator rangerAnim;
+	private Animator tankerAnim;
+
+	void Awake() {
+		Assert.IsNotNull(hero);
+		Assert.IsNotNull(soldier);
+		Assert.IsNotNull(ranger);
+		Assert.IsNotNull(tanker);
+	}
+
 	// Use this for initialization
 	void Start () {
-		
+		heroAnim = hero.GetComponent<Animator>();
+		soldierAnim = soldier.GetComponent<Animator>();
+		rangerAnim = ranger.GetComponent<Animator>();
+		tankerAnim = tanker.GetComponent<Animator>();
+		StartCoroutine(showcase());
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	IEnumerator showcase() {
+		yield return new WaitForSeconds(1f);
+		heroAnim.Play("SpinAttack");
+		yield return new WaitForSeconds(1f);
+		tankerAnim.Play("Attack");
+		yield return new WaitForSeconds(1f);
+		soldierAnim.Play("Attack");
+		yield return new WaitForSeconds(1f);
+		rangerAnim.Play("Attack");
+		yield return new WaitForSeconds(1f);
+		StartCoroutine(showcase());
+	}
+
+	public void Battle() {
+		SceneManager.LoadScene("Battle");
+	}
+
+	public void Quit() {
+		Application.Quit();
 	}
 }
