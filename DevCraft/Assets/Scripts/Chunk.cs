5761b09b9d6002179135f91a0cb6375935c94996
Myles Berueda <myles.berueda@gmail.com>
2017-03-31 19:10:35 -1000

    Added Chunk.cs.

---
+++ DevCraft/Assets/Scripts/Chunk.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chunk : MonoBehaviour {
+
+	private List<Vector3> newVertices = new List<Vector3>();
+	private List<int> newTriangles = new List<int>();
+	private List<Vector2> newUV = new List<Vector2>();
+	private Mesh mesh;
+	private MeshCollider chunkCollider;
+	private int faceCount;
+
+	// Textures
+	private Vector2 grassTop = new Vector2(1, 11);
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
