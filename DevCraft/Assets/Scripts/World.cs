e6e618e006e53e4aa45e3ba1e66ef4c32c7e0c52
Myles Berueda <myles.berueda@gmail.com>
2017-04-01 17:09:52 -1000

    Added World.cs.

---
+++ DevCraft/Assets/Scripts/World.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour {
+
+	[SerializeField] byte[,,] worldData;
+	[SerializeField] int worldX = 16;
+	[SerializeField] int worldY = 16;
+	[SerializeField] int worldZ = 16;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
