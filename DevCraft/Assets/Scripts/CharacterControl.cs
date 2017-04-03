abc14047b8d3ca6b8d858d9eb28f81fc64cfac83
Myles Berueda <myles.berueda@gmail.com>
2017-04-03 08:06:32 -1000

    Added GameManager.cs with getters and setters.

--- DevCraft/Assets/Scripts/CharacterControl.cs
+++ DevCraft/Assets/Scripts/CharacterControl.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using System.Collections;
 
 public class CharacterControl : MonoBehaviour {
 
@@ -16,6 +18,15 @@ public class CharacterControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Vector3 moveChar = new Vector3( CrossPlatformInputManager.GetAxis("Horizontal"), 
+																		0, 
+																		CrossPlatformInputManager.GetAxis("Vertical") );
+		transform.position += moveChar * Time.deltaTime * moveSpeed;
+
+		if (charRigidbody.velocity.magnitude == 0) {
+			anim.SetBool("isWalking", false);
+		} else {
+			anim.SetBool("isWalking", true);
+		}
 	}
 }
---
+++ DevCraft/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager> {
+
+	private bool isJumping = false;
+	private bool isPunching = false;
+	private bool isBuilding = false;
+
+	public bool IsJumping {
+		get { return isJumping; }
+		set { isJumping = value; }
+	}
+
+	public bool IsPunching {
+		get { return isPunching; }
+		set { isPunching = value; }
+	}
+
+	public bool IsBuilding {
+		get { return isBuilding; }
+		set { isBuilding = value; }
+	}
+
+	
+}
