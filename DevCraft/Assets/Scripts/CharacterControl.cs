128182d5d6498426ed6ff6445146d7f6310209cb
Myles Berueda <myles.berueda@gmail.com>
2017-04-03 07:56:54 -1000

    Added CharacterControl.cs.

---
+++ DevCraft/Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CharacterControl : MonoBehaviour {
+
+	[SerializeField] int moveSpeed;
+	[SerializeField] int jumpHeight;
+
+	private Rigidbody charRigidbody;
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		charRigidbody = GetComponent<Rigidbody>();
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
