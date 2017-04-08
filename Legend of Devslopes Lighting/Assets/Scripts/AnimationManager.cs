066ecc58c035878e6b3ab8ded9785ed94e6ad7cb
Myles Berueda <myles.berueda@gmail.com>
2017-04-07 15:27:44 -1000

    Added camera script.

---
+++ Legend of Devslopes Lighting/Assets/Scripts/AnimationManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AnimationManager : MonoBehaviour {
+
+	[SerializeField] private List<GameObject> cameras = new List<GameObject>();
+
+	public static AnimationManager instance = null;
+
+	// Use this for initialization
+	void Start () {
+		if (instance == null)	{
+			instance = this;
+		}	else if (instance != this) {
+			Destroy(gameObject);
+		}
+	}
+
+	void StartNextCamera() {
+		if (cameras.Count > 0) {
+			cameras[0].SetActive(true);
+		}
+	}
+
+	void DestroyCamera(GameObject camera)	{
+		// Remove the first camera from the list.
+		cameras.RemoveAt(0);
+		Destroy(camera);
+		StartNextCamera();
+	}
+}
